6e7e152052bbbfa24750632add43a963119cd837
RADHESWAR REDDY MADIREDDY <m.radhes@gmail.com>
2023-01-03 15:15:59 -0600

    Model Package

--- src/main/java/main/app/controller/ProductController.java
+++ src/main/java/main/app/controller/ProductController.java
@@ -1,12 +1,14 @@
 package main.app.controller;
 
 import main.app.entity.Product;
+import main.app.model.ProductInformation;
 import main.app.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,8 +23,17 @@ public class ProductController {
     }
 
     @GetMapping("/allProduct")
-    public List<Product> getAllProducts(){
-        return (List<Product>) productRepository.findAll();
+    public List<ProductInformation> getAllProducts(){
+        List<Product> productsInDB = (List<Product>) productRepository.findAll();
+        List<ProductInformation> productsWithNameAndPrice = new ArrayList<>();
+        for (Product product : productsInDB){
+            ProductInformation productInformation = new ProductInformation();
+            productInformation.setProductName(product.getName());
+            productInformation.setPrice(product.getPrice());
+            productsWithNameAndPrice.add(productInformation);
+
+        }
+        return productsWithNameAndPrice;
 
     }
 }
